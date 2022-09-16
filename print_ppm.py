151c9dd1d5caaec07c32e5e9e6021f36f2682fc6
Carbou Clément <clement.carbou@edu.univ-eiffel.fr>
2022-09-16 16:46:18 +0200

    Programme qui affiche une image PPM

--- print_ppm.py
+++ print_ppm.py
@@ -2,19 +2,18 @@
 ##------------Affiche l'image d'un fichier PPM ----------##
 ###########################################################
 
-f=open("guybrush3.p3", 'r', encoding="utf-8")
+f = open("guybrush3.p3.ppm", "r", encoding="utf-8")
 
 def affiche_p3(fichier):
     
     ligne=f.readline()
-    x,y=0,0
     
     ## VERIFIE LE FORMAT DU FICHIER ##
     if form(fichier,ligne) != True:
         return ("Format invalide")
     
     ## DEFINIT LA TAILLE DE L'IMAGE ##
-    print(taille(fichier,ligne))
+    x,y=taille(fichier,ligne)
 
     
 
@@ -30,26 +29,32 @@ def form(fichier,ligne):
             p = True
         elif p == True:
             return c == "3"
-        elif c=="#":
+        elif c == "#":
             ligne=f.readline()
  
 
 def taille(fichier,ligne):
-    p=False
-    x=None
     
-    for c in ligne:
-        if c == "P":
-             p=True
-        elif c == "3" and p == True:
-            p=False
-        elif c == "#":
-            ligne=f.readline()
-        else:
-            if x is None:
-                x=c
+    nx=False
+    
+    p=False
+    x=""
+    y=""
+    
+    for a in fichier:
+        for c in a:
+            if c == "P":
+                p=True
+            elif c == "3" and p == True:
+                p=False
+            elif c == "#":
+                ligne=f.readline()
+            elif c == " " and 
             else:
-                return x,c           
+                if x == "":
+                    x.append
+                else:
+                    return x,c           
       
 #def intens_max(fichier,l,i)    
 #def intens(fichier,l,i):
